ec04b096507a805f679a2ad1c3d26d082ee59924
Daniel Mur-Spiegl <news@spiegl.me>
2019-09-24 20:09:50 +0200

    refactor script

--- cognitiveModels/enhanceCognitiveModelWithData.js
+++ cognitiveModels/enhanceCognitiveModelWithData.js
@@ -1,37 +1,32 @@
 /**
- * This script reads data from the `data` directory and transforms it for the LUIS model
  *
- * A template model input will get enhanced.
+ * This script reads data from the `data` directory and files from this directory and transforms it for the LUIS model
  */
 
-/**
- * Config
- */
-
-const inputModelPath = "./cognitiveModels/CognitiveModelBasis.json";
-const outputModelPath = "./cognitiveModels/output.json";
+const fs = require("fs");
+const path = require("path");
 
 /**
  * Init
  */
-console.log("Starting enhancing model ...");
-
-const fs = require("fs");
+console.log("\nStarting enhancing LUIS model ...");
 
 /**
+ * Config
+ */
+const inputModelPath = "./CognitiveModelBasis.json";
+const outputModelPath = "./output.json";
+
+/**
+ * Add entities with sub lists
+ *
  * Read data from data directory
  */
-const rawData = fs.readFileSync("./data/data.json", "utf8");
-data = JSON.parse(rawData);
-
-const countryPropertiesRawData = fs.readFileSync(
-  "./data/countryProperties.json",
-  "utf8"
-);
-countryPropertiesData = JSON.parse(countryPropertiesRawData);
+let data = require("../data/data.json");
+let countryPropertiesData = require("../data/countryProperties.json");
 
 /**
- * Add counties
+ * Add countries
  * Modify data into following format
  *
  * {
@@ -43,6 +38,8 @@ countryPropertiesData = JSON.parse(countryPropertiesRawData);
  *  ]
  * }
  */
+
+console.log("\n\tProcessing countries");
 
 let countries = {
   name: "Country",
@@ -63,11 +60,13 @@ data.map(item => {
 });
 
 // log number of countries
-console.log("\t" + countries.subLists.length + " records modified ...");
+console.log("\t" + countries.subLists.length + " records processed ...");
 
 /**
  * Add CountryProperties
  */
+
+console.log("\n\tProcessing CountryProperties");
 
 let countryProperties = {
   name: "CountryProperty",
@@ -85,6 +84,11 @@ countryPropertiesData.map(item => {
   countryProperties.subLists.push(prop);
 });
 
+// log number of countries
+console.log(
+  "\t" + countryProperties.subLists.length + " records processed ..."
+);
+
 /**
  * Add utterances for QueryKM
  * 
@@ -94,6 +98,8 @@ countryPropertiesData.map(item => {
         "entities": []
     },
  */
+console.log("\n\tProcessing utterances for QueryKM");
+
 let utterancesRaw = [];
 countryPropertiesData.map(item => {
   item.utterances.map(utterance => utterancesRaw.push(utterance));
@@ -106,20 +112,25 @@ utterancesRaw.map(utterance => {
   });
 });
 
-let utterancesList = [];
+let utterancesFilterKM = [];
 utterances.map(item => {
   let utterance = {
     text: item,
     intent: "QueryKM",
     entities: []
   };
-  utterancesList.push(utterance);
+  utterancesFilterKM.push(utterance);
 });
+
+// log number of countries
+console.log("\t" + utterancesFilterKM.length + " records processed ...");
 
 /**
  * Add utterances for FilterQueryKM
  *
  */
+console.log("\n\tProcessing utterances for FilterQueryKM");
+
 let utterancesFilterQueryRaw = require("./utterancesFilterQueryKM.json");
 
 let utterancesFilterQuery = [];
@@ -144,10 +155,15 @@ utterancesFilterQueryRaw.map(item => {
   utterancesFilterQuery.push(utterance);
 });
 
+// log number of countries
+console.log("\t" + utterancesFilterQuery.length + " records processed ...");
+
 /**
  * Add utterances for AggregateQueryKM
  *
  */
+console.log("\n\tProcessing utterances for AggregateQueryKM");
+
 let utterancesAggregateQueryRaw = require("./utterancesAggregateQueryKM.json");
 
 let utterancesAggregateQuery = [];
@@ -172,22 +188,27 @@ utterancesAggregateQueryRaw.map(item => {
   utterancesAggregateQuery.push(utterance);
 });
 
+// log number of countries
+console.log("\t" + utterancesAggregateQuery.length + " records processed ...");
+
 /**
  * Read Template and insert modified data
  */
-let model = JSON.parse(fs.readFileSync("./" + inputModelPath, "utf8"));
+let model = require(inputModelPath);
 
 model.closedLists.push(countries);
 model.closedLists.push(countryProperties);
-model.utterances = model.utterances.concat(utterancesList);
+model.utterances = model.utterances.concat(utterancesFilterKM);
 model.utterances = model.utterances.concat(utterancesFilterQuery);
 model.utterances = model.utterances.concat(utterancesAggregateQuery);
-
-//console.log(model)
 
 /**
  * Write model
  */
 
-fs.writeFileSync(outputModelPath, JSON.stringify(model), "utf8");
-console.log("\tFile written to " + outputModelPath);
+fs.writeFileSync(
+  path.resolve(__dirname, outputModelPath),
+  JSON.stringify(model),
+  "utf8"
+);
+console.log("\n\tFile written to " + outputModelPath);
